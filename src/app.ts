f2fa00ca931ce6fe75f810452848598bfd1bf80e
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-03-18 11:36:46 +0000

    Feat: Let users know the latest API version and the route to it

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,9 @@ import { errorHandler, handle404 } from "./services/error_handler";
 export const app = express();
 
 app.use(express.json());
+app.get("/", (req, res) =>
+  res.send("Latest API is version 1, accessible at /api/v1")
+);
 app.use("/api/v1", router);
 
 app.use(errorHandler);
