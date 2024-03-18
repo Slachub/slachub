22c8dbd9df887b0953db1e5a7ff1ecad62acd0d6
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-03-18 12:06:53 +0000

    Fix: mini bugfix around webhooks error handling

--- src/controllers/webhooks_controller.ts
+++ src/controllers/webhooks_controller.ts
@@ -1,41 +1,36 @@
 import { NextFunction, Response, Request } from "express";
 import BadRequestError from "../services/error_handler";
-
+import { addHookToMgr, verifySignature } from "../services/webhooks";
 import * as slackService from "../services/slackService";
 
-import { addHookToMgr, verifySignature } from "../services/webhooks";
-
-
 export const handleWebhook = async (
-    req: Request,
-    res: Response,
-    next: NextFunction
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    if (verifySignature(req)) {
-        // const hook: Webhook = createHook(req.body);
-        // QueueManager.getInstance().getQueue().enqueue(hook);
-        const hook  = await addHookToMgr(req.body);
-        if(hook)
-            res.status(200).send("Success");        
-    } else {
-        next(
-            new BadRequestError(403, {
-                message: "Unauthorised",
-                details: req.body
-            })
-        );
-    }
+  if (verifySignature(req)) {
+    // const hook: Webhook = createHook(req.body);
+    // QueueManager.getInstance().getQueue().enqueue(hook);
+    const hook = await addHookToMgr(req.body);
+    if (hook) res.status(200).send("Success");
+  } else {
+    next(
+      new BadRequestError(403, {
+        message: "Unauthorised",
+        details: req.body,
+      })
+    );
+  }
 };
 
 export const sendToSlack = async (req: Request, res: Response) => {
-	const payload = req.body;
-	try{		
-	 const status = await slackService.updateToSlack(payload);	 
-	  if(!status) {
-         res.status(400).json(error)
-	  } else
-	     res.status(200).json(status);
-	} catch (error) {
-		res.status(400).json({ message: (error as Error).message });
-	}
+  const payload = req.body;
+  try {
+    const status = await slackService.updateToSlack(payload);
+    if (!status) {
+      res.status(400).json(status);
+    } else res.status(200).json(status);
+  } catch (error) {
+    res.status(400).json({ message: (error as Error).message });
+  }
 };
