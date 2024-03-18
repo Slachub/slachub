6cb91fcf28b1b0f1202d8669d3b870144ffff930
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-03-18 11:46:10 +0000

    Fix: changed path to queue mgr service

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
 import { app } from "./app";
-import { processQueue } from "./queue/queue_mgr";
+import { processQueue } from "./services/queue_mgr_service";
 
 // Start the worker function
 processQueue().catch((err) => {
-    console.error("Error in worker:", err);
+  console.error("Error in worker:", err);
 });
 
 app.listen(3000, () => {
-    console.log("Express server started on port 3000");
+  console.log("Express server started on port 3000");
 });
