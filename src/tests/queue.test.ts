068aa423265970bf8a0246a8158f50e80ab3ca29
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-03-19 18:18:29 +0000

    Fix: Update queue tests for new model

--- src/tests/queue.test.ts
+++ src/tests/queue.test.ts
@@ -5,16 +5,18 @@ import { QueueManager, processQueue } from "../services/queue_mgr_service";
 import { queueHook } from "../services/webhooks_service";
 
 describe("adding to queue", () => {
-  const queue: Queue<Webhook> = new Queue();
-  const hook: Webhook = createHook({ action: PR_HOOK_OPENED_1.action });
-  queue.enqueue(hook);
+  it("should add to queue", () => {
+    const queue: Queue<Webhook> = new Queue();
+    const hook: Webhook = createHook(PR_HOOK_OPENED_1);
+    queue.enqueue(hook);
 
-  expect(queue.peek()).toEqual(hook);
+    expect(queue.peek()).toEqual(hook);
+  });
 });
 
 describe("test queue mgr", () => {
   const queue = QueueManager.getInstance().getQueue();
-  const hook: Webhook = createHook({ action: PR_HOOK_OPENED_1.action });
+  const hook: Webhook = createHook(PR_HOOK_OPENED_1);
 
   it("should have one item in the queue", () => {
     queue.enqueue(hook);
@@ -30,9 +32,9 @@ describe("test queue mgr", () => {
 
 describe("integration test of addHookToMgr", () => {
   it("should return a valid hook", async () => {
-    const hook: Webhook = await queueHook(PR_HOOK_CLOSED_1);
+    const hook: Webhook = await queueHook(PR_HOOK_OPENED_1);
 
-    expect(hook.action).toEqual(PR_HOOK_CLOSED_1.action);
+    expect(hook.action).toEqual(PR_HOOK_OPENED_1.action);
     expect(new Date(hook.created_at)).toBeInstanceOf(Date);
   });
 });
