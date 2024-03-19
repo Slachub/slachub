54761e22d6f0b801d876a14c25b4b59e00152cb3
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-03-19 16:07:57 +0000

    Refactor: indentation

--- src/services/queue_mgr_service.ts
+++ src/services/queue_mgr_service.ts
@@ -3,56 +3,52 @@ import { Queue } from "../models/queue_model";
 
 // Singleton pattern for Queue instance
 export class QueueManager {
-    private static instance: QueueManager;
-    private queue: Queue<Webhook>;
-    private processedHooks: Array<Webhook>;
-    private receivedHooks: Array<Webhook>;
+  private static instance: QueueManager;
+  private queue: Queue<Webhook>;
+  private processedHooks: Array<Webhook>;
+  private receivedHooks: Array<Webhook>;
 
-    private constructor() {
-        this.queue = new Queue<Webhook>(); 
-        this.processedHooks =  new Array<Webhook>();
-        this.receivedHooks =  new Array();
-    }
+  private constructor() {
+    this.queue = new Queue<Webhook>();
+    this.processedHooks = new Array<Webhook>();
+    this.receivedHooks = new Array();
+  }
 
-    public static getInstance(): QueueManager {
-        if (!QueueManager.instance) {
-            QueueManager.instance = new QueueManager();
-        }
-        return QueueManager.instance;
+  public static getInstance(): QueueManager {
+    if (!QueueManager.instance) {
+      QueueManager.instance = new QueueManager();
     }
+    return QueueManager.instance;
+  }
 
-    public getQueue(): Queue<Webhook> {
-        return this.queue;
-    }
+  public getQueue(): Queue<Webhook> {
+    return this.queue;
+  }
 
-    public getProcessedHooks(): Array<Webhook> {
-        return this.processedHooks;
-    }
+  public getProcessedHooks(): Array<Webhook> {
+    return this.processedHooks;
+  }
 
-    public getReceivedHooks(): Array<any> {
-        return this.receivedHooks;
-    }
+  public getReceivedHooks(): Array<any> {
+    return this.receivedHooks;
+  }
 }
 
 // Worker function to process items from the queue
 export const processQueue = async (): Promise<void> => {
-    console.log("Starting Queue Mgr...");
-    const manager = QueueManager.getInstance();
-    const queue = manager.getQueue();
+  console.log("Starting Queue Mgr...");
+  const manager = QueueManager.getInstance();
+  const queue = manager.getQueue();
 
-    while (true) {
-        if (!queue.isEmpty()) {
-            const item = queue.dequeue();
-            if (item) {
-                console.log("Processing item:", item);
+  while (true) {
+    if (!queue.isEmpty()) {
+      const item = queue.dequeue();
+      if (item) {
+        console.log("Processing item:", item);
 
-                try{
-                    // Call slackService here
-                }catch{}
-
-                manager.getProcessedHooks().push(item);
-            }
-        }
-        await new Promise((resolve) => setTimeout(resolve, 250)); // Process item every 250ms. Can remove when system is oerational.
+        manager.getProcessedHooks().push(item);
+      }
     }
+    await new Promise((resolve) => setTimeout(resolve, 250)); // Process item every 250ms. Can remove when system is oerational.
+  }
 };
