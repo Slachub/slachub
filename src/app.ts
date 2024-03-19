cb20faaadcb7d6d24229d157a9c9cd4123f27b22
Stuart Basden <mystery@Stuarts-Air.broadband>
2024-03-19 15:35:47 +0000

    Feat: Add a /health endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { router } from "./routes/routes";
 import { errorHandler, handle404 } from "./services/error_handler";
+import serverStatus from "express-now";
 
 export const app = express();
+
+app.use("/health", serverStatus(app));
 
 app.use(express.json());
 app.use("/api/v1", router);
